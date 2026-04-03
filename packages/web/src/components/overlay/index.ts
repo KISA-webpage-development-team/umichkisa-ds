078c9488132188e0d0af03108c9aaabe3a25ff3d
retz8 <ekkicb71@gmail.com>
2026-04-03 18:49:31 +0900

    feat(dialog): export DialogDescription, DialogFooter and all types

--- packages/web/src/components/overlay/index.ts
+++ packages/web/src/components/overlay/index.ts
@@ -1,5 +1,5 @@
-export { Dialog, DialogTrigger, DialogClose, DialogContent, DialogTitle } from "./Dialog";
-export type { DialogContentProps, DialogTitleProps } from "./Dialog";
+export { Dialog, DialogTrigger, DialogClose, DialogContent, DialogTitle, DialogDescription, DialogFooter } from "./Dialog";
+export type { DialogProps, DialogTriggerProps, DialogCloseProps, DialogContentProps, DialogTitleProps, DialogDescriptionProps, DialogFooterProps } from "./Dialog";
 export { Dropdown, DropdownTrigger, DropdownContent, DropdownItem, DropdownGroup, DropdownSeparator } from "./Dropdown";
 export type { DropdownProps, DropdownTriggerProps, DropdownContentProps, DropdownItemProps, DropdownGroupProps } from "./Dropdown";
 export { Popover, PopoverTrigger, PopoverContent } from "./Popover";
