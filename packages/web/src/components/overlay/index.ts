949830915456993f4ad2f89cb539baedaaf82919
retz8 <ekkicb71@gmail.com>
2026-04-03 16:03:09 +0900

    feat(dropdown): rewrite with DS tokens, groups, separator, destructive variant

--- packages/web/src/components/overlay/index.ts
+++ packages/web/src/components/overlay/index.ts
@@ -1,6 +1,6 @@
 export { Dialog, DialogTrigger, DialogClose, DialogContent, DialogTitle } from "./Dialog";
 export type { DialogContentProps, DialogTitleProps } from "./Dialog";
-export { Dropdown, DropdownTrigger, DropdownContent, DropdownItem } from "./Dropdown";
-export type { DropdownContentProps, DropdownItemProps } from "./Dropdown";
+export { Dropdown, DropdownTrigger, DropdownContent, DropdownItem, DropdownGroup, DropdownSeparator } from "./Dropdown";
+export type { DropdownProps, DropdownTriggerProps, DropdownContentProps, DropdownItemProps, DropdownGroupProps } from "./Dropdown";
 export { Popover, PopoverTrigger, PopoverContent } from "./Popover";
 export type { PopoverContentProps } from "./Popover";
