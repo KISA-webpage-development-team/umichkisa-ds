05bc9fbb777ee73638f049133beb3cbd23551552
retz8 <ekkicb71@gmail.com>
2026-04-05 16:04:06 +0900

    fix(docs): migrate ColorSwatchGrid to DS Grid component

--- apps/docs/components/ColorSwatchGrid.tsx
+++ apps/docs/components/ColorSwatchGrid.tsx
@@ -1,11 +1,17 @@
+import { Grid } from '@umichkisa-ds/web'
+
 type ColorSwatchGridProps = {
   children: React.ReactNode
 }
 
 export function ColorSwatchGrid({ children }: ColorSwatchGridProps) {
   return (
-    <div className="my-6 grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-4">
+    <Grid
+      columns={{ base: 2, md: 3, lg: 4 }}
+      gap="element"
+      className="my-6"
+    >
       {children}
-    </div>
+    </Grid>
   )
 }
